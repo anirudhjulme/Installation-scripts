a05a5aa38cc7ed27c0ee5b519aa3abaaad6a4162
Tarun Julmi <128776656+anirudhjulme@users.noreply.github.com>
2023-06-20 10:07:27 +0530

    Update Jenkins.sh
    New Way of installing Jenkins have been added

--- Jenkins.sh
+++ Jenkins.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-sudo apt update -y
 
-sudo apt install default-jre -y
-
-java -version
-
-sudo apt update -y
-
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-
-sudo apt update -y
-
-sudo add-apt-repository universe -y
-
+sudo apt update -y 
+sudo apt install openjdk-11-jre -y
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
 sudo apt-get install jenkins -y
-
-sudo service jenkins start
-
-cat /var/lib/jenkins/secrets/initialAdminPassword
